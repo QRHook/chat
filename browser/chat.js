e50a7b5af9a2e999a13f4a704728c53d9aa8d0a7
Jarrett Cruger <jcruger@qrhook.com>
2013-01-03 20:50:59 -0500

    beginning to put chat box creation together

--- browser/chat.js
+++ browser/chat.js
@@ -1,4 +1,5 @@
 // Author: Jarrett Cruger -> QR Hook
+//
 // Module for handling and creating chat box objects
 
 var html = require('./html/chat');
@@ -7,5 +8,11 @@ module.exports = Chat;
 
 function Chat (target) {
     if(!(this instanceof Chat)) return new Chat(target);
+    this.target = target;
+    this.chats = [];
+}
+
+Chat.prototype.create = Create;
+function Create (data) {
 
 }
--- browser/html/chat.js
+++ browser/html/chat.js
@@ -3,6 +3,8 @@ module.exports = [
         '<div class="text-view">',
         '</div>',
         '<div class="message-bar">',
+            '<input type="text">',
+            '<button class="send-message" type="button"/>',
         '</div>',
     '</div>'
 ].join('\n');
